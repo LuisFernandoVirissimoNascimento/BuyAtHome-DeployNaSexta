36a86c0df8f680a54e08e4f7158ef80a309bf668
Luis Fernando <luisvirissimo789@gmail.com>
2025-07-15 10:51:52 -0400

    Almost done

--- Views/welcome.php
+++ Views/welcome.php
@@ -3,17 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="public/images/icon.ico" type="image/x-icon">
     <title>Bem-vindo</title>
     <link rel="stylesheet" href="public/Assets/Css/welcome.css">
     <link rel="stylesheet" href="public/Assets/Css/style.css">
 </head>
 <body>
 <section class="header-background">
-  <div class="header-background-content">
-    <h1>Mini-mercado de Condomínio</h1>
-    <p>Uma facilidade que surpreende.</p>
-    <button>Resgatar Cupons</button>
-  </div>
+    <div class="header-background-content">
+        <div class="left-items">
+            <h1>Mini-mercado de Condomínio</h1>
+            <h1 class="orange-text">Uma facilidade que surpreende.</h1>
+        </div>
+        <div class="right-items">
+            <button>Resgatar Cupons</button>
+        </div>    
+    </div>
 </section>
+<div class="division"></div>
+<div class="center">
+    <h1>PromoBuy</h1>
+    <h1 class="orange-text-title">É muito mais economia!</h1>    
+</div>
+<div class="paded">
+    <div class="products-container">
+        <div class="product">Product 1</div>
+        <div class="product">Product 2</div>
+        <div class="product">Product 3</div>
+        <div class="product">Product 4</div>
+    </div>  
+</div>
+
 </body>
 </html>
