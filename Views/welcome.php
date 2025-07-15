e65b4c0580f74ddfbf0b22e5d0cf31ab1b3d809c
Enzo <enzoglopez1@gmail.com>
2025-07-15 08:59:31 -0400

    atualizando branch

--- Views/welcome.php
+++ Views/welcome.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bem-vindo</title>
     <link rel="stylesheet" href="../public/CSS/welcome.css">
 </head>
+
 <body>
-<section class="hero">
-  <div class="hero-content">
-    <h1>Mini-mercado de Condomínio</h1>
-    <p>Uma facilidade que surpreende.</p>
-    <button>Resgatar Cupons</button>
-  </div>
-</section>
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Mini-mercado de Condomínio</h1>
+            <p>Uma facilidade que surpreende.</p>
+            <button>Resgatar Cupons</button>
+        </div>
+    </section>
 </body>
+
 </html>
