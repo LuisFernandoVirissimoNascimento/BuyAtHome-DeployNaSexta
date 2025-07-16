5f7421c9c42bb7ea66a20a40ad2535dc09c3a09a
Vatreax <rafasmontiel13@gmail.com>
2025-07-16 10:08:10 -0400

    feat: criando uma seleção para os cards

--- Views/lista_descontos.php
+++ Views/lista_descontos.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/Assets/Css/descontos.css">
     <link rel="icon" href="public/images/icon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="public/Assets/Css/style.css">
     <link rel="stylesheet" href="public/Assets/Css/base.css">
     <link rel="stylesheet" href="public/Assets/Css/navbar.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
@@ -16,10 +17,9 @@
     <navbar></navbar>
     <div class="content">
         <h1>Veja quais <a>descontos</a> você pode adquirir!</h1>
-
-        <cardDesconto class="container-desconto"></cardDesconto>
-    
-        <button class="btn-desconto">Comprar</button>
+            <cardDesconto class="container-desconto"></cardDesconto>
+        
+            <button class="btn-desconto">Comprar</button>
     </div>
 
 </body>
